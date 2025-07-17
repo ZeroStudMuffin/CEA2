00d38bc158c4a97eaf346210f38254b2de2bfbe1
ZeroStudMuffin <78259671+ZeroStudMuffin@users.noreply.github.com>
2025-07-17 12:57:42 -0700

    Update CheckoutUploader.kt

--- app/src/main/java/com/example/app/CheckoutUploader.kt
+++ app/src/main/java/com/example/app/CheckoutUploader.kt
@@ -42,7 +42,7 @@ object CheckoutUploader {
                         "Content-Type",
                         "application/x-www-form-urlencoded"
                     )
-                    setRequestProperty("X-API-Key", "4!qCj8eD@kP7z#hFwL$yR6bN*tE2s")
+                    setRequestProperty("X-API-Key", "4!qCj8eD@kP7z#hFwL%yR6bN*tE2s")
                 }
 
                 val body = buildString {
